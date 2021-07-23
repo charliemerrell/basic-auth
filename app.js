45b9c54377a88f348cce986ebf678c8d9818bec5
Charlie Merrell <charlie.merrell@multiverse.io>
2021-07-23 16:29:16 +0100

    adds basic authentication

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ const app = express();
 
 app.use(express.json());
 
+async function verifyUser(req, res, next) {
+    const authHeader = req.headers.authorization;
+    const base64 = authHeader.split(" ")[1];
+    const usernamePassword = String(Buffer.from(base64, "base64"));
+    const [username, password] = usernamePassword.split(":");
+    const userRecord = await User.findOne({
+        where: {
+            username,
+        },
+    });
+    if (!userRecord) {
+        return res.sendStatus(404);
+    }
+    const passwordIsCorrect = await bcrypt.compare(
+        password,
+        userRecord.passwordHash
+    );
+    if (passwordIsCorrect) {
+        next();
+    } else {
+        return res.sendStatus(403);
+    }
+}
+
+app.get("/secrets", verifyUser, (req, res) => {
+    res.send("shhhh this was secret");
+});
+
 app.post("/users", async (req, res) => {
     const { username, password } = req.body;
     const passwordHash = await bcrypt.hash(password, 10);
